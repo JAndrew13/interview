38346dd0d99ef6607ba0cfc6e83684f1fc7638d8
michael.paulus <michael.paulus@icevonline.com>
2024-05-06 13:49:09 -0400

    updates

--- src/AdventureWorks/Controllers/HomeController.cs
+++ src/AdventureWorks/Controllers/HomeController.cs
@@ -7,15 +7,46 @@ namespace AdventureWorks.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public HomeController()
+        {
+            _applicationDbContext = ApplicationDbContext.Create();
+        }
+
+
+        public HomeController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
         public ActionResult Index()
         {
             return View();
         }
 
-        [Authorize]
-        public async Task<ActionResult> About()
+        [AllowAnonymous]
+        public ActionResult CreateUsers()
         {
-            var userService = new UserService(ApplicationDbContext.Create());
+            var userService = new UserService(_applicationDbContext);
+            var count = userService.GetUserCount();
+
+            ViewBag.Message = $"{count} users.";
+
+            if ((System.Web.HttpContext.Current?.User.Identity.IsAuthenticated).GetValueOrDefault())
+            {
+                ViewBag.Message += $"  {System.Web.HttpContext.Current.User.Identity.Name} is logged in";
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> CreateUsersPost()
+        {
+            var userService = new UserService(_applicationDbContext);
+            
             await Task.Run(() =>
             {
 
@@ -26,16 +57,13 @@ namespace AdventureWorks.Controllers
 
             ViewBag.Message = $"{count} users.";
 
-            ViewBag.Message += $"  {System.Web.HttpContext.Current?.User.Identity.Name} is logged in";
+            if ((System.Web.HttpContext.Current?.User.Identity.IsAuthenticated).GetValueOrDefault())
+            {
+                ViewBag.Message += $"  {System.Web.HttpContext.Current.User.Identity.Name} is logged in";
+            }
 
-            return View();
+            return View("CreateUsers");
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
