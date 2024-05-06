efd92b2966200ef870e4891822edf4d2c0df3888
michael.paulus <michael.paulus@icevonline.com>
2024-05-06 07:24:07 -0400

    init commit

---
+++ src/AdventureWorks/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdventureWorks.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+            await Task.Run(() => 
+            {
+                //do something
+            }).ConfigureAwait(false);
+            if ((System.Web.HttpContext.Current?.User.Identity.IsAuthenticated).GetValueOrDefault())
+            {
+                ViewBag.Message = $"{System.Web.HttpContext.Current.User.Identity.Name} is logged in";
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
